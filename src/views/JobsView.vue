bda790e3e4d916c2f76c01a7be2bf31d3564e5c3
aixenhuh <aixenhuh@gmail.com>
2019-06-16 23:59:12 +0900

    commit

---
+++ src/views/JobsView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div v-for="job in this.$store.state.jobs">{{ job.title }}</div>
+    </div>
+</template>
+
+<script>
+import { fetchJobsList } from '../api/index.js'
+
+export default {
+    created() {
+        this.$store.dispatch('FETCH_JOBS');
+    }
+}
+</script>
+
+<style>
+
+</style>
+ 
---
+++ src/views/NewsView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div v-for='user in this.$store.state.news' > {{ user.title }}</div>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    created() {
+        this.$store.dispatch('FETCH_NEWS');
+        // var vm = this;
+        // fetchNewsList().then(function(response) {
+        //     vm.users = response.data;
+        // })
+        // .catch(function(err) {
+        //     console.log(err);
+        // })
+    },
+}
+</script>
+
+<style>
+
+</style>
