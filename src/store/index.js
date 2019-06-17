1b0419c287c6c8b7bf35e91177d243f45dde9799
aixenhuh <aixenhuh@gmail.com>
2019-06-17 22:53:34 +0900

    commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vuex from 'vuex';
 import Vue from 'vue';
-import {fetchNewsList,fetchAskList,fetchJobsList} from '../api/index.js'
+import mutations from './mutations.js'
+import actions from './actions.js'
 
 Vue.use(Vuex);
 
@@ -10,34 +11,14 @@ export const store = new Vuex.Store({
         ask : [],
         jobs : []
     },
-    mutations : {
-        SET_NEWS (state, news){
-            state.news = news;
+    getters: {
+        fetchedAsk (state) {
+            return state.ask;
         },
-        SET_ASK (state, ask){
-            state.ask = ask;
-        },
-        SET_JOBS (state, jobs){
-            state.jobs = jobs;
+        fetchedNews (state) {
+            return state.news;
         }
     },
-    actions: {
-        FETCH_NEWS(context) {
-            fetchNewsList().then(response=> {
-                context.commit('SET_NEWS', response.data);
-            }).catch(error => {
-                console.log(error);
-            })
-        },
-        FETCH_ASK(context) {
-            fetchAskList().then(response => {
-                context.commit('SET_ASK', response.data);
-            }).catch(err => console.log(err));
-        },
-        FETCH_JOBS({commit}) {
-            fetchJobsList().then(({data}) => {
-                commit('SET_JOBS', data);
-            }).catch(err => console.log(err))
-        }
-    }
+    mutations,
+    actions
 });
--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,12 +1,27 @@
 <template>
     <div>
-        <div v-for="item in this.$store.state.ask">{{ item.title }}</div>
+        <div v-for="item in fetchedAsk">{{ item.title }}</div>
     </div>
 </template>
 
 <script>
-import { fetchAskList } from '../api/index.js'
+import { mapState, mapGetters } from 'vuex';
 export default {
+    computed : {
+        ...mapGetters([
+            'fetchedAsk' 
+        ]),
+        // #2
+        // ...mapState({
+        //     ask : state => state.ask
+        // }),
+
+        // #1
+        // ask() {
+        //     return this.$store.state.ask;
+        // }
+        
+    },
     created() {
         this.$store.dispatch('FETCH_ASK')
     }
