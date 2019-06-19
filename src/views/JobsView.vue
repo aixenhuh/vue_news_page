aa3c2c0b6486fd1dbdcf0d8380702b9c5adf96b6
aixenhuh <aixenhuh@gmail.com>
2019-06-19 22:19:55 +0900

    commit

--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,15 +1,45 @@
 <template>
     <div>
+<<<<<<< HEAD
         <list-item></list-item>
+=======
+        <ul class="news-list">
+            <li v-for="job in fetchedJobs" class="post">
+                <!-- 포인트 영역 -->
+                <div class="points"> 
+                    {{ job.points || 0 }}
+                </div>
+
+                <!-- 기타 정보 영역 -->
+                <div>
+                    <p class="news-title">
+                        <a :href="job.url">{{ job.title }}</a>
+                    </p>
+                    <small class="link-text">{{job.time_ago}} by
+                        <router-link v-bind:to="`/user/${job.user}`" class="link-text">{{ job.user }}</router-link>
+                        <a :href="job.url">{{job.domain}}</a>
+                    </small>
+                </div>
+
+            </li>
+        </ul>
+>>>>>>> commit
     </div>
 </template>
 
 <script>
 import { mapState, mapGetters } from 'vuex'
+<<<<<<< HEAD
 import ListItem from '../components/ListItem.vue'
 export default {
     components : {
         ListItem
+=======
+
+export default {
+    computed : {
+        ...mapGetters(['fetchedJobs'])
+>>>>>>> commit
     },
     created() {
         this.$store.dispatch('FETCH_JOBS');
@@ -17,5 +47,38 @@ export default {
 }
 </script>
 
+<<<<<<< HEAD
 <style scoped></style>
+=======
+<style scoped>
+.news-list {
+    margin:0;
+    padding : 0;
+}
+
+.post {
+    list-style : none;
+    display : flex;
+    align-items : center;
+    border-bottom : 1px solid #eee;
+    padding-left : 10px;
+}
+.points {
+    width : 80px;
+    height : 60px;
+    display : flex;
+    align-items : center;
+    justify-content: center;
+    color : #42b883
+}
+
+.news-title {
+    margin : 0;
+}
+
+.link-text {
+    color : #828282
+}
+</style>
+>>>>>>> commit
  
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,16 +1,45 @@
 <template>
     <div>
+<<<<<<< HEAD
         <list-item></list-item>
+=======
+        <ul class="news-list">
+            <li v-for='item in fetchedNews' class="post">
+                <!-- 포인트 영역 -->
+                <div class="points">
+                    {{ item.points }}
+                </div>
+
+                <!-- 기타 정보 영역 -->
+                <div>
+                    <p class="news-title">
+                        <a :href="item.url">{{ item.title }}</a>
+                    </p>
+                    <small class="link-text">{{item.time_ago}} by
+                        <router-link v-bind:to="`/user/${item.user}`" class="link-text">{{ item.user }}</router-link>
+                    </small>
+                </div>
+
+            </li>
+        </ul>
+>>>>>>> commit
     </div>
 </template>
 
 <script>
+<<<<<<< HEAD
 import ListItem from '../components/ListItem.vue';
 import bus from '../utils/bus.js';
 
 export default {
     components : {
         ListItem,
+=======
+import { mapGetters } from 'vuex';
+export default {
+    computed : {
+        ...mapGetters(['fetchedNews'])
+>>>>>>> commit
     },
     created() {
         bus.$emit('start:spinner');
@@ -20,4 +49,38 @@ export default {
 }
 </script>
 
+<<<<<<< HEAD
 <style scoped></style>
+=======
+<style scoped>
+.news-list {
+    margin:0;
+    padding : 0;
+}
+
+.post {
+    list-style : none;
+    display : flex;
+    align-items : center;
+    border-bottom : 1px solid #eee;
+}
+
+.points {
+    width : 80px;
+    height : 60px;
+    display : flex;
+    align-items : center;
+    justify-content: center;
+    color : #42b883
+}
+
+.news-title {
+    margin : 0;
+}
+
+.link-text {
+    color : #828282
+}
+
+</style>
+>>>>>>> commit
